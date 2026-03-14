a0b51aec56b3cb940f4e7eb5e041cb3ab3888d11
SaGgaSsa <saggas.apoc@gmail.com>
2026-03-14 21:21:22 +0000

    Fix Prisma startup command in Docker

--- daily-tasks/docker/app/start.sh
+++ daily-tasks/docker/app/start.sh
@@ -2,7 +2,7 @@
 set -eu
 
 echo "[app] Esperando base de datos..."
-until ./node_modules/.bin/prisma db push --skip-generate >/tmp/prisma-start.log 2>&1; do
+until ./node_modules/.bin/prisma db push >/tmp/prisma-start.log 2>&1; do
   cat /tmp/prisma-start.log
   sleep 3
 done
