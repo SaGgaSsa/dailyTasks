ba13cb4a40f8df86c21faf0d6f5ade9fb8d7889c
SaGgaSsa <saggas.apoc@gmail.com>
2026-03-17 01:26:26 +0000

    fix migration

--- daily-tasks/docker/app/start.sh
+++ daily-tasks/docker/app/start.sh
@@ -4,8 +4,5 @@ set -eu
 echo "[app] Sincronizando schema..."
 ./node_modules/.bin/prisma db push
 
-echo "[app] Ejecutando seed..."
-./node_modules/.bin/prisma db seed
-
 echo "[app] Iniciando app..."
 exec node server.js
