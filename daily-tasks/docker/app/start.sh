7d63e101eb04a29005c37a2d01e2668732bd2aac
SaGgaSsa <98482583+SaGgaSsa@users.noreply.github.com>
2026-03-17 21:30:27 -0300

    Update start.sh

--- daily-tasks/docker/app/start.sh
+++ daily-tasks/docker/app/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-#echo "[app] Aplicando migraciones..."
-#./node_modules/.bin/prisma m
+echo "[app] Aplicando migraciones..."
+./node_modules/.bin/prisma migrate deploy
 
 echo "[app] Iniciando app..."
 exec node server.js
